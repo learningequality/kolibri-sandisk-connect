5ffa488c53ae2dae9eb8046bc7222bdea6eac0f9
Rafael Aguayo <newuser@user-VirtualBox.(none)>
2016-02-17 21:01:52 -0800

    Added GUI for Sandisk Configuration.

---
+++ automator/automator_GUI.py
@@ -0,0 +1,120 @@
+from Tkinter import *
+import tkMessageBox
+import ttk
+from ScrolledText import *
+
+from subprocess import Popen
+import subprocess
+import os
+import time
+import random
+
+from workers import WorkerThread
+from wifi import get_wipi_list
+from wifi import get_server_list
+
+
+class Application(Frame):
+    def submit(self, sandisk_id, nb):
+	# if string does not meet requirements, do nothing
+	if not sandisk_id or len(sandisk_id) != 4:
+		return False
+
+        result = tkMessageBox.askyesno("SSID Confirmation", "Is SSID %s correct?" % sandisk_id)
+	if result:
+		# create notebook tab, and update GUI
+		page = ttk.Frame(nb)
+		text = ScrolledText(page)
+		text.pack(expand=1, fill="both")
+		nb.add(page, text='%s (LOADING)' % sandisk_id)
+		self.SSID_submit.config(state=DISABLED)
+		tab_id = len(nb.tabs()) - 1
+		nb.select(tab_id)
+		self.update_idletasks()
+		
+		# find available sandisks, else show GUI error
+		sandisk_list = get_server_list()
+		if sandisk_id not in sandisk_list:
+			tkMessageBox.showerror("SSID Unavailable", "SanDisk ID: %s not in list of available SanDisks.\nPlease check connection or SanDisk ID." % sandisk_id)
+			nb.forget(tab_id)
+			self.SSID_submit.config(state=NORMAL)
+			return
+
+		# find available wipis, else show GUI error
+		wipi_list = get_wipi_list()
+		if len(wipi_list) == 0:
+			tkMessageBox.showerror("Wipis Unavailable", "There are no available wipis.")
+			nb.forget(tab_id)
+			self.SSID_submit.config(state=NORMAL)
+			return
+
+		# remove wipis and sandisks in use
+		for worker in WorkerThread.get_active():
+			if worker.sandisk_id in sandisk_list:
+				sandisk_list.remove(worker.sandisk_id)
+			if worker.wipi_name in wipi_list: 
+				wipi_list.remove(worker.wipi_name)
+		
+		# choose random wipi and assign id
+		wipi_name = random.choice(wipi_list)	
+		self.wipi_dict[wipi_name] = len(self.wipi_dict) + 1
+		
+		# update tab name
+		nb.tab(tab_id, text=sandisk_id)		
+
+		#Create worker thread with this pair
+		thread = WorkerThread(wipi_name, sandisk_id, text, self.wipi_dict, nb)
+
+		self.SSID_submit.config(state=NORMAL)
+
+		Button(page, text='Close Tab', command=lambda: self.closeTab(page, thread)).pack(side=LEFT)
+
+		#Start the thread
+		thread.start()
+
+		time.sleep(5)
+
+    def test(self):
+	for i in range(10):
+		print "Hello!"
+		time.sleep(10)
+
+    def closeTab(self, tab, th):
+	# prevent user from closing tab if thread is active
+	#if th.isAlive():
+	#	tkMessageBox.showerror('Closing Tab Error', 'Process is still running.\n Please wait for it to finish.')
+	#	return
+	# close tab on user request
+	result = tkMessageBox.askyesno('Close Tab Confirmation', 'Are you sure want to close the current tab?')
+	if result:
+		tab.destroy()
+
+    def createWidgets(self):
+	# SSID label widget
+	self.SSID_label = Label(self, text="Please Enter SSID:")
+	self.SSID_label.pack({"side": "left"})
+	
+	# User supplied string widget
+	SSID = StringVar()
+	self.SSID_entry = Entry(self, textvariable=SSID)
+	self.SSID_entry.pack({"side": "left"})
+
+	# add notebook to GUI
+	nb = ttk.Notebook(self)
+
+	# submit button which updates GUI
+	self.SSID_submit = Button(self, text="Submit", command=lambda: self.submit(SSID.get(), nb) )
+	self.SSID_submit.pack({"side": "left"})
+
+	nb.pack(expand=1, fill="both")
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+	self.wipi_dict = dict()
+
+root = Tk()
+root.wm_title("Sandisk Configuration")
+app = Application(master=root)
+app.mainloop()
--- automator/wifi.py
+++ automator/wifi.py
@@ -3,7 +3,7 @@ import re			#Regular expression syntax formats
 
 def get_server_list():
 	#scan for wifi
-	proc = subprocess.Popen('sudo iwlist wipi1 scan 2>/dev/null', shell=True, stdout=subprocess.PIPE,)
+	proc = subprocess.Popen('sudo iwlist wipi8 scan 2>/dev/null', shell=True, stdout=subprocess.PIPE)
 	#displays outputs of list
 	stdout_str = proc.communicate()[0]
 	#parse the outputs
@@ -26,12 +26,10 @@ def get_wipi_list():
 	stdout_list = stdout_str.split('\n')
 	#empty array to save the parsed outputs
 	wipi_ids=[]
-	#search for wipi adapters that are available
-	for line in stdout_list:
-		line=line.strip()
-		match=re.search('wipi(\d+)', line)
-		if match:
-			wipi_ids.append(int(match.group(1))) 
-	if 1 in wipi_ids:
-		wipi_ids.remove(1)
+	# search for wipi adapters that are available
+	output = subprocess.check_output("ifconfig | grep '00:c1:41' | awk '{print $1}'", shell=True);
+	output_list = output.split()
+	for line in output_list:
+		line = line.strip()
+		wipi_ids.append(line)
 	return wipi_ids
