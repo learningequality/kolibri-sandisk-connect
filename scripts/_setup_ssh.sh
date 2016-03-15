bd37e3ca523b9d5da1737b40b13e512966d0a2fc
Your Name <you@example.com>
2016-03-14 19:38:16 -0700

    Updated code to work with latest kalite tar file and materials folder. Updated GUI code to to handle the tabs for each thread more efficiently.

--- scripts/_setup_ssh.sh
+++ scripts/_setup_ssh.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/expect
-set HOST 192.168.11.1
 set USER root
-set PASSWD [lindex $argv 0]
+set IP [lindex $argv 0]
+set PASSWD [lindex $argv 1]
 
-spawn telnet $HOST
+spawn telnet $IP
 
 expect "Media_Drive login:"
 send "$USER\r"
@@ -16,3 +16,7 @@ send "sed -r -i '1s/(\[a-zA-Z\]:)0/\\1999999/' /etc/shadow\n"
 
 expect "Media_Drive"
 send "/etc/init.d/service/sshd start\n"
+
+expect "Media_Drive"
+send "exit\n"
+interact
